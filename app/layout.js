ea9567b9e409e3e87fcbb1d46c3e5b8c6eae2084
littAmui <samuel.amui@leanicontechnology.com>
2023-11-23 13:23:05 +0000

    setup the real estate project

---
+++ app/layout.js
@@ -0,0 +1,21 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+import { Providers } from "@/redux/provider";
+
+export const metadata = {
+  title: "msaada",
+  description: "Best platform to monitor your spending",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,11 @@
+'use client'
+
+import React from "react";
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
+
+function Home() {
+  return <div>Home</div>;
+}
+
+export default Home;
