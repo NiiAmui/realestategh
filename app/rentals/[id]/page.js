d78747e3b616d4924d7a7a7608aec6c56655363f
littAmui <samuel.amui@leanicontechnology.com>
2024-01-04 00:05:08 +0000

    minor additions to the rental detail page

--- app/rentals/[id]/page.js
+++ app/rentals/[id]/page.js
@@ -5,6 +5,7 @@ import Image from "next/image";
 
 import RatingsAndBadge from "@/components/rentals/RatingsAndBadge";
 import ReserveActionCard from "@/components/rentals/ReserveActionCard";
+import { WifiIcon } from "@heroicons/react/24/outline";
 
 const imagesURL = [
   "https://a0.muscache.com/im/pictures/miso/Hosting-668146487515150072/original/8ff2a532-e0cd-41a2-9164-554c4d9eb28a.jpeg?im_w=720",
@@ -56,29 +57,132 @@ const Page = ({ params }) => {
         {/* see all images section */}
         <div className=" w-[300px] min-h-full flex justify-center">
           {/* Btn to see all images */}
-          <button className="btn rounded border-[1.5px] px-4 py-2 text-gray-600 cursor-pointer">See all images</button>
+          <button className="btn rounded border-[1.5px] px-4 py-2 text-gray-600 cursor-pointer">
+            See all images
+          </button>
         </div>
       </div>
 
       {/* rent meta data */}
       <div className="rentMetaData flex items-center divide-x gap-2 mt-4 text-gray-500 text-sm">
-            {/* bedrooms */}
-            <p>{3} bedroom{true && 's'}</p>
-            {/* Meter */}
-            <p className="pl-2">{'Personal'} Meter</p>
-            {/* Bathrooms */}
-            <p className="pl-2">{2} bathrooms</p>
+        {/* bedrooms */}
+        <p>
+          {3} bedroom{true && "s"}
+        </p>
+        {/* Meter */}
+        <p className="pl-2">{"Personal"} Meter</p>
+        {/* Bathrooms */}
+        <p className="pl-2">{2} bathrooms</p>
       </div>
 
       {/* content section of the details page */}
       <div className="contentSection mt-4  grid grid-cols-3 gap-20">
         {/* left sides */}
         <section className=" col-span-2 ">
-            <RatingsAndBadge />
+          <RatingsAndBadge />
+          {/* LordLord details */}
+          <div className="landLordName mt-10 flex items-center gap-4 border-b pb-6">
+            {/* image */}
+            <Image
+              src={
+                "https://a0.muscache.com/im/pictures/user/3816a6ef-5cc9-40ed-ae47-406646daa103.jpg?im_w=240"
+              }
+              alt="image"
+              width={50}
+              height={50}
+              className="rounded-full"
+            />
+            {/* name and date joined */}
+            <div className="landLordDetails">
+              {/* title */}
+              <p>Rented by Kwamena</p>
+              {/* date joined  */}
+              <p className="text-gray-500 text-sm">2 years on realestategh</p>
+            </div>
+          </div>
+
+          {/* about the place */}
+          <div className="rentalDescription mt-10">
+            {/* title */}
+            <p className="font-semibold text-lg">About this place</p>
+            {/* description */}
+            <div>
+              Enjoy an elegant 20 m2 private room in a renovated 160 m2
+              apartment in the heart of Nantes city center in the popular
+              'Graslin' district in Nantes. The charm of the old renovated:
+              ceiling height of 3.60 m, period parquet, black marble fireplace,
+              comfortable bathroom.
+            </div>
+          </div>
+
+          {/* facilities and amenities */}
+          <div className="mt-10">
+            {/* title */}
+            <p className="font-semibold text-lg">What this place Offers</p>
+
+            {/* offerings */}
+            <div className="mt-2 grid grid-cols-2 gap-3">
+              {/* facility */}
+              <div className="flex items-center gap-4">
+                {/* logo */}
+                <WifiIcon className="w-4"/>
+                {/* title */}
+                <p>
+                  Free Wifi
+                </p>
+              </div>
+              {/* facility */}
+              <div className="flex items-center gap-4">
+                {/* logo */}
+                <WifiIcon className="w-4"/>
+                {/* title */}
+                <p>
+                  Free Laundry
+                </p>
+              </div>
+              {/* facility */}
+              <div className="flex items-center gap-4">
+                {/* logo */}
+                <WifiIcon className="w-4"/>
+                {/* title */}
+                <p>
+                  Poolside
+                </p>
+              </div>
+              {/* facility */}
+              <div className="flex items-center gap-4">
+                {/* logo */}
+                <WifiIcon className="w-4"/>
+                {/* title */}
+                <p>
+                  Garden
+                </p>
+              </div>
+              {/* facility */}
+              <div className="flex items-center gap-4">
+                {/* logo */}
+                <WifiIcon className="w-4"/>
+                {/* title */}
+                <p>
+                  Security Camera
+                </p>
+              </div>
+              {/* facility */}
+              <div className="flex items-center gap-4">
+                {/* logo */}
+                <WifiIcon className="w-4"/>
+                {/* title */}
+                <p>
+                  Personal Balcony
+                </p>
+              </div>
+            </div>
+          </div>
         </section>
+
         {/* right sides */}
         <section className="">
-            <ReserveActionCard />
+          <ReserveActionCard />
         </section>
       </div>
     </div>
